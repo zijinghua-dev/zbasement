96fbad64b888614b56423ae0be0edc9954f277a6
蒋昱 <149087821@qq.com>
2020-09-01 19:00:52 +0800

    no message

--- publishable/configs/code.php
+++ publishable/configs/code.php
@@ -905,6 +905,69 @@ return [
                 ],
             ],
         ],
-    ]
+    ],
+    'view' => [
+        'submit'=>[
+            'success' => [
+                'http_code' => 200,
+                'code' => 'ZBASEMENT_CODE_VIEW_SUBMIT_SUCCESS',
+                'status' => true,
+                'message' => '查看结果操作运行成功!'
+            ],
+            'failed' => [
+                'http_code' => 403,
+                'code' => 'ZBASEMENT_CODE_VIEW_SUBMIT_FAILED',
+                'status' => false,
+                'message' => '查看结果操作运行失败!'
+            ],
+        ],
+
+        'validation' => [
+            'failed'=>[
+                'http_code' => 422,
+                'code' => 'ZBASEMENT_CODE_VIEW_VALIDATION_FAILED',
+                'status' => false,
+                'message' => '查看结果操作输入参数验证失败!'
+            ],
+            'success'=>[
+                'http_code' => 200,
+                'code' => 'ZBASEMENT_CODE_VIEW_VALIDATION_SUCCESS',
+                'status' => true,
+                'message' => '查看结果操作输入参数验证成功!'
+            ],
+        ],
+        'load'=>[
+            'rules'=>[
+                'success'=>[
+                    'http_code' => 200,
+                    'code' => 'ZBASEMENT_CODE_VIEW_LOAD_RULES_SUCCESS',
+                    'status' => true,
+                    'message' => '查看结果操作所需验证规则加载成功!'
+                ],
+                'failed'=>[
+                    'http_code' => 403,
+                    'code' => 'ZBASEMENT_CODE_查看结果操作_LOAD_RULES_FAILED',
+                    'status' => false,
+                    'message' => '查看结果操作所需验证规则加载失败!'
+                ],
+
+            ],
+            'messages'=>[
+                'success'=>[
+                    'http_code' => 200,
+                    'code' => 'ZBASEMENT_CODE_VIEW_LOAD_MESSAGES_SUCCESS',
+                    'status' => true,
+                    'message' => '查看结果操作所需验证规则的提示信息加载成功!'
+                ],
+                'failed'=>[
+                    'http_code' => 403,
+                    'code' => 'ZBASEMENT_CODE_VIEW_LOAD_MESSAGES_FAILED',
+                    'status' => false,
+                    'message' => '查看结果操作所需验证规则的提示信息加载失败!'
+                ],
+
+            ],
+        ],
+    ],
 
 ];
